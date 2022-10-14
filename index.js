a8994e8e2c9be7899bb488e7024936f56f1077b2
Sakar Masoud <masoudsakar@gmail.com>
2022-10-14 14:29:28 +0300

    Sakar Masoud

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
+    const bt1 = document.getElementById('button');
+    bt1.addEventListener('click', clickAlert);
+}
+function clickAlert() {
+    alert('I was clicked!');
 }
